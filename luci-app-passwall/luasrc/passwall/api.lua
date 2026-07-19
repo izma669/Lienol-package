3159494f42b7d5de4bafe44ee597254616f6efdc
ZXH <42003399+izma669@users.noreply.github.com>
2026-07-19 21:00:27 +0800

    Add files via upload

--- luci-app-passwall/luasrc/passwall/api.lua
+++ luci-app-passwall/luasrc/passwall/api.lua
@@ -229,7 +229,7 @@ function curl_auto(url, file, args)
 end
 
 function url(...)
-	local url = string.format("admin/services/%s", appname)
+	local url = string.format("admin/vpn/%s", appname)
 	local args = { ... }
 	for i, v in pairs(args) do
 		if v ~= "" then
